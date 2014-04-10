c8f99fddf0f00d0ca8d35b144b18cfc2d883c987
root <marcelodf12@gmail.com>
2014-04-10 11:24:29 -0400

    ABM Proyecto completo

--- aps/aplicaciones/proyectos/urls.py
+++ aps/aplicaciones/proyectos/urls.py
@@ -1,8 +1,19 @@
 from django.conf.urls import patterns, url
-from aps.aplicaciones.proyectos.views import crearProyecto, adminProyecto, listarProyectos
+from aps.aplicaciones.proyectos.views import \
+    crearProyecto, \
+    adminProyecto, \
+    listarProyectos, \
+    modificarProyectos, \
+    eliminarProyectos, \
+    listarProyectosNoIniciados, \
+    iniciarProyecto
 
 urlpatterns = patterns('',
     url(r'^crear/$', crearProyecto.as_view(), name='crear_proyecto'),
     url(r'^admin/$', adminProyecto.as_view(), name='admin_proyecto'),
     url(r'^listar/$', listarProyectos.as_view(), name='listar_proyectos'),
+    url(r'^modificar/(?P<id>\d+)$', modificarProyectos.as_view(), name='modificar_proyectos'),
+    url(r'^eliminar/(?P<id>\d+)$', eliminarProyectos.as_view(), name='eliminar_proyectos'),
+    url(r'^iniciar/(?P<id>\d+)$', iniciarProyecto.as_view(), name='iniciar_proyecto'),
+    url(r'^noIniciados/$', listarProyectosNoIniciados.as_view(), name='listar_proyectos_no_iniciados'),
 )
--- aps/aplicaciones/proyectos/views.py
+++ aps/aplicaciones/proyectos/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView, FormView
 from aps.aplicaciones.proyectos.models import Proyectos
 from django.core.urlresolvers import reverse_lazy
+from aps.aplicaciones.proyectos.forms import ComentariosLog
 
 # Create your views here.
 class crearProyecto(CreateView):
     template_name = 'proyectos/crearProyecto.html'
     model = Proyectos
     success_url = reverse_lazy('listar_proyectos')
+    #fields =
+
+    def form_valid(self, form):
+        proyecto = form.save()
+        proyecto.estado='creado'
+        proyecto.save()
+        return super(crearProyecto, self).form_valid(form)
 
 class adminProyecto(TemplateView):
     template_name = 'proyectos/admin.html'
@@ -16,3 +24,40 @@ class listarProyectos(ListView):
     template_name = 'proyectos/verProyectos.html'
     model = Proyectos
     context_object_name = 'proyectos'
+
+class listarProyectosNoIniciados(ListView):
+    template_name = 'proyectos/listanoiniciados.html'
+    queryset = Proyectos.objects.filter(estado='creado')
+    context_object_name = 'proyectos'
+
+class modificarProyectos(UpdateView):
+    model = Proyectos
+    fields = ['nombre']
+    template_name = 'proyectos/update.html'
+    success_url = reverse_lazy('listar_proyectos')
+
+    def get_object(self, queryset=None):
+        obj = Proyectos.objects.get(id=self.kwargs['id'])
+        return obj
+
+class eliminarProyectos(FormView):
+    form_class = ComentariosLog
+    template_name = 'proyectos/eliminar.html'
+    success_url = reverse_lazy('listar_proyectos')
+
+    def form_valid(self, form):
+        proyecto = Proyectos.objects.get(id=self.kwargs['id'])
+        proyecto.estado='eliminado'
+        proyecto.save()
+        return super(eliminarProyectos, self).form_valid(form)
+
+class iniciarProyecto(FormView):
+    form_class = ComentariosLog
+    template_name = 'proyectos/iniciar.html'
+    success_url = reverse_lazy('listar_proyectos')
+
+    def form_valid(self, form):
+        proyecto = Proyectos.objects.get(id=self.kwargs['id'])
+        proyecto.estado='activo'
+        proyecto.save()
+        return super(iniciarProyecto, self).form_valid(form)
