37d135d781ae513b3d8e4281711e89ac87b81dbb
root <marcelodf12@gmail.com>
2014-04-12 00:50:00 -0400

    Testing 1.0

--- aps/aplicaciones/inicio/tests.py
+++ aps/aplicaciones/inicio/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+"""
+    Modulo de pruebas para la aplicacion inicio
+"""
+from django.utils import unittest
+from django.test.client import Client
 
-# Create your tests here.
+class TestDeRespuestaDeServidor(unittest.TestCase):
+    """
+        Prueba para comprobar si el servidor responde correctamente
+    """
+    def setUp(self):
+        # Creacion de un cliente
+        self.client = Client()
+
+    def test_details(self):
+        # Simula un GET del cliente
+        response = self.client.get('/inicio/')
+        usuario=response.context['user']
+
+        # La solicitud tiene exito si el servidor responde 200
+        self.assertEqual(response.status_code, 200)
+
+class TestDeAutenticacion(unittest.TestCase):
+    """
+        Prueba para comprobar si el index inicia con un usuario no autenticado
+    """
+    def setUp(self):
+        # Creacion de un cliente
+        self.client = Client()
+
+    def test_details(self):
+        # Simula un GET del cliente
+        response = self.client.get('/inicio/')
+        usuario=response.context['user']
+
+        # Cheque que se ingreso al index pero no se ha autenticado
+        self.assertEqual(usuario.__unicode__(), "AnonymousUser")
+
+if __name__ == '__main__':
+    unittest.main()
+
--- aps/aplicaciones/proyectos/tests.py
+++ aps/aplicaciones/proyectos/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+"""
+    Modulo de Test para el Modelo  Proyecto
+"""
+from django.utils import unittest
+from aps.aplicaciones.proyectos.models import Proyectos
+from datetime import datetime
 
-# Create your tests here.
+class TestAltaProyecto(unittest.TestCase):
+    """
+        Test que simula un alta de Proyecto
+    """
+    def setUp(self):
+        self.p1= Proyectos()
+        self.p1.nombre = "P1"
+        self.p1.fechaInicio = datetime.today()
+        self.p1.cantFases = 1
+        self.p1.estado = "creado"
+        self.p1.save()
+        self.p2= Proyectos()
+        self.p2.nombre = "P2"
+        self.p2.fechaInicio = datetime.today()
+        self.p2.cantFases = 1
+        self.p2.estado = "creado"
+        self.p2.save()
+
+    def test_details(self):
+         self.assertEqual(self.p1.nombre, "P1")
+         self.assertEqual(self.p2.nombre, "P2")
+
+if __name__ == '__main__':
+    unittest.main()
