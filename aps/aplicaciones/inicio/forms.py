24d6f7d8fa007bff2c69d3ae78db3cd26745494d
marcelo <mar26celo@hotmail.com>
2014-04-11 22:44:18 -0400

    Documentacion 3.0

--- aps/aplicaciones/inicio/forms.py
+++ aps/aplicaciones/inicio/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class UserForm(UserCreationForm):
-    """ ALGO AQUI """
+    """ Definicion del formulario personalizado UserForm """
     nombre = forms.Field()
     apellido = forms.Field()
     correo = forms.Field()
--- aps/aplicaciones/inicio/views.py
+++ aps/aplicaciones/inicio/views.py
@@ -20,16 +20,15 @@ class home(TemplateView):
 
 class Registrarse(FormView):
     """ Vista para registrar un usuario, hereda atributos y metodos de la clase FormView """
-     #No posee ninguna vista asociada?????????????????
 
     template_name = 'inicio/registro.html'
-    form_class = UserForm                   # ???????????
+    form_class = UserForm                   # Formulario a utilizar para la vista
     success_url = reverse_lazy('login')     # Se mostrara la vista 'login' en el caso de registro exitoso
 
     def form_valid(self, form):
         """ Se extiende la funcion form_valid, se agrega el codigo adicional de abajo a la funcion original """
 
-        user = form.save()          # Se guardan los datos del formulario en 'item'????????
+        user = form.save()          # Se crea un nuevo user de django
         usuarioNuevo = Usuario()    # Se crea un objeto de tipo Usuario
 
         # Se guarda los datos del formulario en el objeto 'UsuarioNuevo' creado anteriormente
