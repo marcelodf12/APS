02bc57006330333761a1f079fbfc8a2e46b5a914
root <marcelodf12@gmail.com>
2014-04-14 15:50:07 -0400

    Modificación del modelo Usuario: Borrado.

--- aps/aplicaciones/inicio/admin.py
+++ aps/aplicaciones/inicio/admin.py
@@ -1,5 +1,2 @@
 from django.contrib import admin
-from aps.aplicaciones.inicio.models import Usuario
 
-# Register your models here.
-admin.site.register(Usuario)
--- aps/aplicaciones/inicio/views.py
+++ aps/aplicaciones/inicio/views.py
@@ -8,8 +8,7 @@ from django.views.generic import TemplateView
 from django.views.generic import FormView
 from aps.aplicaciones.inicio.forms import UserForm
 from django.core.urlresolvers import reverse_lazy
-from aps.aplicaciones.inicio.models import Usuario  # se importan los modelos definidos en el archivo MODELS.py
-
+from django.contrib.auth.models import User
 
 
 class home(TemplateView):
@@ -27,15 +26,9 @@ class Registrarse(FormView):
 
     def form_valid(self, form):
         """ Se extiende la funcion form_valid, se agrega el codigo adicional de abajo a la funcion original """
-
-        user = form.save()          # Se crea un nuevo user de django
-        usuarioNuevo = Usuario()    # Se crea un objeto de tipo Usuario
-
-        # Se guarda los datos del formulario en el objeto 'UsuarioNuevo' creado anteriormente
-        usuarioNuevo.user = user
-        usuarioNuevo.nombre = form.cleaned_data['nombre']
-        usuarioNuevo.apellido = form.cleaned_data['apellido']
-        usuarioNuevo.correo = form.cleaned_data['correo']
-        usuarioNuevo.estado = 'activo'
-        usuarioNuevo.save()
+        user = form.save()
+        user.first_name = form.cleaned_data['nombre']
+        user.last_name = form.cleaned_data['apellido']
+        user.email = form.cleaned_data['correo']
+        user.save()
         return super(Registrarse, self).form_valid(form)
