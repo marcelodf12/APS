82c5215965e570490ef21df16f6aa05a1466678e
root <marcelodf12@gmail.com>
2014-04-15 11:25:41 -0400

    Modificar Contraseña Usuarios

--- aps/aplicaciones/inicio/views.py
+++ aps/aplicaciones/inicio/views.py
@@ -6,10 +6,11 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 from django.views.generic import FormView
-from aps.aplicaciones.inicio.forms import UserForm
+from django.views.generic import UpdateView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
-
+from aps.aplicaciones.inicio.forms import UserForm, ActualizarPass
+from django.shortcuts import render, HttpResponseRedirect
 
 class home(TemplateView):
     """ Vista de bienvenida (login exitoso), hereda atributos y metodos de la clase TemplateView """
@@ -32,3 +33,40 @@ class Registrarse(FormView):
         user.email = form.cleaned_data['correo']
         user.save()
         return super(Registrarse, self).form_valid(form)
+
+class UpdateUser(UpdateView):
+    model = User
+    template_name = "inicio/modificar.html"
+    success_url = reverse_lazy('inicio')
+    fields= ['first_name', 'last_name','email']
+
+    def get_object(self, queryset=None):
+        """ Se extiende la funcion get_object, se agrega el codigo adicional de abajo a la funcion original """
+        obj = User.objects.get(id=self.kwargs['id'])
+        return obj
+
+
+class ActualizarPassView(FormView):
+    form_class = ActualizarPass
+    template_name = 'inicio/modificarPassword.html'
+    success_url = reverse_lazy('inicio')
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        usuario=request.user
+        if form.is_valid():
+            return self.form_valid(form, usuario, **kwargs)
+        else:
+            return self.form_invalid(form, **kwargs)
+
+    def form_valid(self, form, usuario):
+        nombre=form.cleaned_data['usuario']
+        passAnt=(form.cleaned_data['passAnt'])
+        pass1=form.cleaned_data['pass1']
+        pass2=form.cleaned_data['pass2']
+        if pass1==pass2:
+            if(usuario.check_password(passAnt)):
+                usuario.set_password(pass1)
+                usuario.save()
+        return HttpResponseRedirect('/inicio/')
