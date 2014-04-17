4b531d62d3616279d2a891ab368cb97791ec353f
root <marcelodf12@gmail.com>
2014-04-17 14:35:19 -0400

    Estilos 5.0 y Permisos Beta 2.0

--- aps/aplicaciones/permisos/models.py
+++ aps/aplicaciones/permisos/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 from aps.aplicaciones.proyectos.models import Proyectos
 from aps.aplicaciones.fases.models import fases
 from aps.aplicaciones.items.models import items
 
 
 # Create your models here.
+TIPOS_PERMISOS = (
+    ('proyecto','proyecto'),
+    ('fase','fase'),
+    ('item','item'),
+    ('permiso','permiso'),
+)
+CLASE_PERMISOS = (
+    ('VER','VER'),
+    ('ADD','ADD'),
+    ('MODIFICAR','MODIFICAR'),
+    ('DELETE','DELETE'),
+)
 class Permisos(models.Model):
-    permiso = models.CharField(max_length=30)
-    tipoObjeto = models.CharField(max_length=15)
-    usuario = models.ForeignKey(User)
-    proyecto = models.ForeignKey(Proyectos, null='true')
-    fases = models.ForeignKey(fases, null='true')
-    items = models.ForeignKey(items, null='true')
+    permiso = models.CharField(max_length=30, choices=CLASE_PERMISOS)
+    tipoObjeto = models.CharField(max_length=15, choices=TIPOS_PERMISOS)
+    usuario = models.ForeignKey(User, null='True', blank='True')
+    grupo = models.ForeignKey(Group, null='True', blank='True')
+    proyecto = models.ForeignKey(Proyectos, null='True', blank='True')
+    fases = models.ForeignKey(fases, null='True', blank='True')
+    items = models.ForeignKey(items, null='True', blank='True')
 
     def __unicode__(self):
         return self.permiso
-
-    @staticmethod
-    def nuevo(permiso, tipoObjeto, objeto, usuario):
-        pass
