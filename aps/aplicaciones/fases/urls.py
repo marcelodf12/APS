ed58141839b3e7b1f8b64a1d9dce2bbc21c00b23
marcelo <mar26celo@hotmail.com>
2014-05-01 12:06:30 -0400

    URL arreglado, funciona agregar fases en proyecto

--- aps/aplicaciones/fases/urls.py
+++ aps/aplicaciones/fases/urls.py
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
     url(r'^listar/', listarFases.as_view(), name='listar_fases'),
     url(r'^modificar/(?P<id>\d+)$', modificarFases.as_view(), name='modificar_fases'),
     url(r'^eliminar/(?P<id>\d+)$', eliminarFase.as_view(), name='eliminar_fase'),
-    url(r'^crearEnProyecto/$', crearFaseEnProyecto.as_view(), name='crear_fase_en_proyecto'),
+    url(r'^crearEnProyecto/(?P<id>\d+)$', crearFaseEnProyecto.as_view(), name='crear_fase_en_proyecto'),
 )
