a000557ffa426be795870ad304277f253e9408ed
marcelo <mar26celo@hotmail.com>
2014-05-14 10:00:56 -0400

    Creando modelos para manejo de versiones en items Creacion html para versiones

--- aps/aplicaciones/items/models.py
+++ aps/aplicaciones/items/models.py
@@ -23,3 +23,22 @@ class items(models.Model):
         """ Metodo llamado para visualizar objetos. En este caso se mostrara el valor del atributo 'nombre' para
          cada instancia del modelo items """
         return self.nombre
+
+
+
+class items_versiones(models.Model):
+    """Este es el modelo 'items_versiones' con sus atributos, al sincronizar con la Base de Datos la clase se convertira
+    en una tabla y sus atributos se traduciran en campos de la misma. """
+
+    item = models.ForeignKey(items, null='True')
+    version = models.IntegerField(default='1')
+
+
+class atributo_version(models.Model):
+    """Este es el modelo 'atributo_version' con sus atributos, al sincronizar con la Base de Datos la clase se convertira
+    en una tabla y sus atributos se traduciran en campos de la misma. """
+
+    itemVersion = models.ForeignKey(items_versiones, null='True')
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200)
+    estado = models.CharField(max_length=15, default='creado')
