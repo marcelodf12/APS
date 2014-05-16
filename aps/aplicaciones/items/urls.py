8622e76e6fe113c58df53594048c5a451ca370e0
marcelo <mar26celo@hotmail.com>
2014-05-16 09:56:02 -0400

    modificacion de fases (para posibilitar creacion de fases) pruebas unitarias varias, pendiente: atributos y relaciones....

--- aps/aplicaciones/items/urls.py
+++ aps/aplicaciones/items/urls.py
@@ -23,12 +23,10 @@ urlpatterns = patterns('',
     url(r'^relaciones/crear/$', crearRelacion.as_view(), name='crear_relacion'),
     url(r'^relaciones/listar/(?P<id>\d+)$', listarRelaciones.as_view(), name='listar_relaciones'),
     url(r'^relaciones/eliminar/(?P<id>\d+)$', eliminarRelacion.as_view(), name='eliminar_relaciones'),
-
     url(r'^atributos/agregar/(?P<id>\d+)$', agregarAtributo.as_view(), name='agregar_atributo'),
     url(r'^atributos/listar/(?P<id>\d+)$', mostrarDetalles.as_view(), name='detalles_Item'),
     url(r'^atributos/listar/(?P<id>\d+)/(?P<idV>\d+)$', mostrarDetallesV.as_view(), name='detalles_Item'),
     url(r'^atributos/listarVersiones/(?P<id>\d+)$', listarVersiones.as_view(), name='listar_versiones'),
-
     url(r'^reversionar/(?P<id>\d+)$', ReversionVersiones.as_view()),
     url(r'^reversionar/$', reversionar.as_view()),
 )
