2d0bf61aef4649984bb708b9357c10b8a259f97a
root <marcelodf12@gmail.com>
2014-05-23 22:44:36 -0400

    Votaciones completo

--- aps/aplicaciones/solicitudCambio/urls.py
+++ aps/aplicaciones/solicitudCambio/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .views import crearSolicitudCambio
+from .views import crearSolicitudCambio, listarVotaciones, votar
 
 admin.autodiscover()
 urlpatterns = patterns('',
    url(r'^crear/(?P<id>\d+)$', crearSolicitudCambio.as_view(), name='crear_solicitud_cambio'),
-
+   url(r'^Votaciones/', listarVotaciones.as_view(), name='listarVotaciones'),
+   url(r'^votar/(?P<id>\d+)$', votar.as_view()),
 )
--- aps/aplicaciones/solicitudCambio/views.py
+++ aps/aplicaciones/solicitudCambio/views.py
@@ -4,9 +4,56 @@ from aps.aplicaciones.lineasBase.models import lineasBase, relacionItemLineaBase
 from aps.aplicaciones.items.models import items
 from aps.aplicaciones.solicitudCambio.models import solicitudCambio, votos
 from aps.aplicaciones.proyectos.models import Proyectos, Miembros
+from datetime import date, timedelta
 from django.core import serializers
 from django.http import HttpResponse
 # Create your views here.
+class listarVotaciones(TemplateView):
+    def get(self, request, *args, **kwargs):
+        usuario = request.user
+        votaciones = votos.objects.filter(usuario=usuario, estado='pendiente')
+        return render(request, 'solicitudCambio/listarVotaciones.html',{'votaciones':votaciones})
+
+class votar(TemplateView):
+    def get(self, request, *args, **kwargs):
+        voto = votos.objects.get(id=kwargs['id'])
+        return render(request, 'solicitudCambio/votar.html', {'voto':voto})
+
+    def post(self, request, *args, **kwargs):
+        voto = votos.objects.get(id=request.POST['id'])
+        if request.POST['voto'] == 'True':
+            voto.aceptar = True
+        else:
+            voto.aceptar = False
+        voto.estado = 'listo'
+        voto.save()
+        faltantes = votos.objects.filter(solicitud=voto.solicitud, estado='pendiente')
+        if not faltantes:
+            aFavor = len(votos.objects.filter(solicitud=voto.solicitud, aceptar=True))
+            enContra = len(votos.objects.filter(solicitud=voto.solicitud, aceptar=False))
+            print 'Fin de la votacion ' + str(aFavor) + 'a favor contra ' + str(enContra) + 'en contra'
+            if aFavor > enContra:
+                voto.solicitud.estado = 'aceptada'
+                voto.solicitud.fechaExpiracion = date.today() + timedelta(days=7)
+            elif enContra > aFavor:
+                voto.solicitud.estado = 'rechazada'
+            else:
+                print 'empate'
+                if votos.objects.get(usuario=voto.solicitud.item.fase.proyecto.lider).aceptar:
+                    print 'lider voto aceptar'
+                    voto.solicitud.estado = 'aceptada'
+                    voto.solicitud.fechaExpiracion = date.today() + timedelta(days=7)
+                else:
+                    print 'lider voto rechazar'
+                    voto.solicitud.estado = 'rechazada'
+            voto.solicitud.save()
+        else:
+            print 'falta todavia'
+        usuario = request.user
+        votaciones = votos.objects.filter(usuario=usuario, estado='pendiente')
+        return render(request, 'solicitudCambio/listarVotaciones.html',{'votaciones':votaciones})
+
+
 class crearSolicitudCambio(TemplateView):
     def get(self, request,*args, **kwargs):
         item = items.objects.get(id= kwargs ['id'])
