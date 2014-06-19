6c6acdaf639ea3533787b34b75bf5770616c70e6
root <marcelodf12@gmail.com>
2014-06-19 17:54:38 -0400

    Informe Solicitud de Cambio Final

--- aps/aplicaciones/reportes/views.py
+++ aps/aplicaciones/reportes/views.py
@@ -19,6 +19,7 @@ class reporteProyecto(TemplateView):
         from reportlab.platypus import SimpleDocTemplate
         from reportlab.platypus import Spacer
         from reportlab.platypus import Table
+        from reportlab.lib import styles
 
         # Importamos clase de hoja de estilo de ejemplo.
         from reportlab.lib.styles import getSampleStyleSheet
@@ -32,10 +33,14 @@ class reporteProyecto(TemplateView):
 
         #Inicializamos la lista Platypus Story.
         story = []
+        # Ahora incluimos una imagen.
+        fichero_imagen = IMAGENES_ROOT + "/cabecera.png"
+        imagen_logo = Image(os.path.realpath(fichero_imagen),width=400,height=50)
+        story.append(imagen_logo)
+
 
         #Definimos como queremos que sea el estilo de la PageTemplate.
-        print estiloHoja
-        cabecera = estiloHoja['Heading4']
+        cabecera = estiloHoja['Heading5']
 
         #No se hara un salto de pagina despues de escribir la cabecera (valor 1 en caso contrario).
         cabecera.pageBreakBefore=0
@@ -43,22 +48,36 @@ class reporteProyecto(TemplateView):
         # Se quiere que se empiece en la primera pagina a escribir. Si es distinto de 0 deja la primera hoja en blanco.
         cabecera.keepWithNext=0
 
-        # Color de la cabecera.
-        cabecera.backColor=colors.lightblue
 
-        # Ahora incluimos una imagen.
-        fichero_imagen = IMAGENES_ROOT + "/cabecera.png"
-        imagen_logo = Image(os.path.realpath(fichero_imagen),width=400,height=100)
-        story.append(imagen_logo)
+
+        # Color de la cabecera.
+        cabecera.backColor=colors.white
+        cabecera.spaceAfter = 0
+        cabecera.spaceBefore = 0
+
+        parrafo = Paragraph('.',cabecera)
+        story.append(parrafo)
+        parrafo = Paragraph('Proyecto: '+ p.nombre,cabecera)
+        story.append(parrafo)
+        parrafo = Paragraph('-'*193,cabecera)
+        story.append(parrafo)
 
         # Incluimos un Flowable, que en este caso es un parrafo.
-        parrafo = Paragraph("Solicitudes de cambio",cabecera)
+
+        cabecera2 = estiloHoja['Heading3']
+        cabecera2.pageBreakBefore=0
+        cabecera2.keepWithNext=0
+        cabecera2.backColor=colors.white
+
+        parrafo = Paragraph('   ',cabecera2)
         # Lo incluimos en el Platypus story.
         story.append(parrafo)
 
         # Definimos un parrafo. Vamos a crear un texto largo para demostrar como se genera mas de una hoja.
         lista = []
-        lista.append(['Id','Linea Base','Descricipcion','Estado','Solicitante', 'Costo', 'Lider Voto'])
+        lista.append(['SOLICITUDES DE CAMBIO','','','','', '', ''])
+        lista.append([' ',' ',' ',' ',' ', ' ', ' '])
+        lista.append(['ID','LINEA BASE','DESCRIPCION','ESTADO','SOLICITANTE', 'COSTO', 'LIDER VOTO?'])
         fas = fases.objects.filter(proyecto=p)
         for s in solicitudes:
             voto=votos.objects.get(solicitud=s, usuario=p.lider)
@@ -68,9 +87,18 @@ class reporteProyecto(TemplateView):
                 votolider = 'Si'
             lista.append([s.id,s.lineaBase.nombre ,s.descripcion, s.estado, s.usuario, s.costoAdicional, votolider])
         t=Table( lista, style = [
-                       ('GRID',(0,0),(-1,-1),0.5,colors.grey),
-                       ('BOX',(0,0),(-1,-1),2,colors.black),
-                       ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+                       ('GRID',(0,0),(-1,-1),0.5,colors.white),
+                       ('BOX',(0,0),(-1,-1),2,colors.white),
+                       ('SPAN',(0,0),(-1,0)),
+                       ('ROWBACKGROUNDS', (0, 3), (-1, -1), (colors.Color(0.9, 0.9, 0.9),colors.white)),
+                       ('BACKGROUND', (0, 2), (-1, 2), colors.rgb2cmyk(r=6,g=62,b=193)),
+                       ('BACKGROUND', (0, 1), (-1, 1), colors.white),
+                       ('LINEABOVE',(0,0),(-1,0),1.5,colors.black),
+                       ('LINEBELOW',(0,0),(-1,0),1.5,colors.black),
+                       ('SIZE',(0,0),(-1,0),12),
+                       ('ALIGN', (0, 0), (-1, 0), 'CENTER'),
+                       ('VALIGN', (0, 0), (-1, 0), 'MIDDLE'),
+                       ('TEXTCOLOR', (0, 2), (-1, 2), colors.white),
                        ]
               )
         # Y lo incluimos en el story.
@@ -80,9 +108,19 @@ class reporteProyecto(TemplateView):
         story.append(Spacer(0,20))
 
         # Creamos un DocTemplate en una hoja DIN A4, en la que se muestra el texto enmarcado (showBoundary=1) por un recuadro.
-        doc=SimpleDocTemplate(MEDIA_ROOT + "/solicitud_" +str(s.id) + ".pdf",pagesize=A4,showBoundary=1)
+        doc=SimpleDocTemplate(MEDIA_ROOT + "/solicitud_" +str(s.id) + ".pdf",pagesize=A4, rightMargin=1, leftMargin=1, topMargin=0, bottomMargin=0)
+        import datetime
+
+        parrafo = Paragraph('-'*193,cabecera)
+        story.append(parrafo)
+        print 'a' + ' '*100 + 'b'
+        parrafo = Paragraph('Fin del Informe' + ' '*100 + '('+str(datetime.date.today()) + ')' ,cabecera)
+        story.append(parrafo)
+
+
 
         # Construimos el Platypus story.
         doc.build(story)
+
         image_data = open(MEDIA_ROOT + "/solicitud_" +str(s.id) + ".pdf", "rb").read()
         return HttpResponse(image_data, mimetype="application/pdf")
